ceab46b4f86b6ffd8060a944e49c92ed7a190e2a
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-04-26 09:14:25 +0100

    MAIN - Mobile product images swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,22 @@ function openColseMobileMenu() {
     const openMenuBtn = document.querySelector('#open-menu');
     const closeMenuBtn = document.querySelector('#close-menu');
 
-    openMenuBtn.addEventListener('click', ()=> {
+    openMenuBtn.addEventListener('click', () => {
         mobileMenu.classList.remove('hidden');
     });
 
-    closeMenuBtn.addEventListener('click', ()=>{
+    closeMenuBtn.addEventListener('click', () => {
         mobileMenu.classList.add('hidden');
     });
 }
 
 openColseMobileMenu();
+
+
+// Mobile product gallery
+var swiper = new Swiper(".mobileProductGallery", {
+    navigation: {
+        nextEl: ".product-swiper-button-next",
+        prevEl: ".product-swiper-button-prev",
+    },
+});
